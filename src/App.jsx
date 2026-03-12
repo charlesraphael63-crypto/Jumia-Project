0bc8a99f893a03606b95be127f96bf7a4865870e
Raphael Oluwapelumi john <charlesraphael63-crypto@example.com>
2026-03-12 12:32:20 +0100

    I added header componet and style it

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css"
+import Header from "./Components/Header"
  import React from 'react'
  
  const App = () => {
    return (
      <div>
-       
+       <Header />
      </div>
    )
  }
---
+++ src/Components/Header.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+        <main className="mainHeader">
+            <div className="menuHeader">
+                <img className="topImg" src="https://ng.jumia.is/cms/0-5-TechWeek/2026/Brand-day/12-march-itel/BD_Desktop_top_strip.gif" alt="image1"></img>
+            </div>
+            <section className="secHeader">
+                <div className="leftLogo">
+                    <img className="imgLogo" src="data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24'%3E%3Cpath fill='%23fff' d='m19.38 9.54-1.85 1.8-1.84 1.8.87 5.08-4.56-2.4-4.56 2.4.87-5.08-1.84-1.8-1.85-1.8 5.1-.74L12 4.18l2.28 4.62zm.34-3.9A9.98 9.98 0 0 0 12 2a9.98 9.98 0 0 0-7.72 3.64A9.95 9.95 0 0 0 2 12a9.95 9.95 0 0 0 3.64 7.71A9.96 9.96 0 0 0 12 22c2.42 0 4.63-.86 6.36-2.29a10 10 0 0 0 1.93-2.12 9.95 9.95 0 0 0 1.71-5.6 9.96 9.96 0 0 0-2.28-6.35'/%3E%3C/svg%3E" alt="image2" />
+                    <h6>Sell on jumia</h6>
+                </div>
+                <div className="rightLogo">
+                    <h5>JUMIA</h5>
+                    <img className="imgLogo" src="data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24'%3E%3Cpath fill='%23fff' d='m19.38 9.54-1.85 1.8-1.84 1.8.87 5.08-4.56-2.4-4.56 2.4.87-5.08-1.84-1.8-1.85-1.8 5.1-.74L12 4.18l2.28 4.62zm.34-3.9A9.98 9.98 0 0 0 12 2a9.98 9.98 0 0 0-7.72 3.64A9.95 9.95 0 0 0 2 12a9.95 9.95 0 0 0 3.64 7.71A9.96 9.96 0 0 0 12 22c2.42 0 4.63-.86 6.36-2.29a10 10 0 0 0 1.93-2.12 9.95 9.95 0 0 0 1.71-5.6 9.96 9.96 0 0 0-2.28-6.35'/%3E%3C/svg%3E" alt="image2" />
+                    <h5 className="pay">PAY</h5>
+                    <img className="imgLogo" src="data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24'%3E%3Cpath fill='%23fff' d='m19.38 9.54-1.85 1.8-1.84 1.8.87 5.08-4.56-2.4-4.56 2.4.87-5.08-1.84-1.8-1.85-1.8 5.1-.74L12 4.18l2.28 4.62zm.34-3.9A9.98 9.98 0 0 0 12 2a9.98 9.98 0 0 0-7.72 3.64A9.95 9.95 0 0 0 2 12a9.95 9.95 0 0 0 3.64 7.71A9.96 9.96 0 0 0 12 22c2.42 0 4.63-.86 6.36-2.29a10 10 0 0 0 1.93-2.12 9.95 9.95 0 0 0 1.71-5.6 9.96 9.96 0 0 0-2.28-6.35'/%3E%3C/svg%3E" alt="image2" />
+                    <h5>DELIVERY</h5>
+                </div>
+            </section>
+            <header>
+                <nav >
+                    <h1>Jumia</h1>
+                    <img className="imgLogo" src="data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24'%3E%3Cpath fill='%23fff' d='m19.38 9.54-1.85 1.8-1.84 1.8.87 5.08-4.56-2.4-4.56 2.4.87-5.08-1.84-1.8-1.85-1.8 5.1-.74L12 4.18l2.28 4.62zm.34-3.9A9.98 9.98 0 0 0 12 2a9.98 9.98 0 0 0-7.72 3.64A9.95 9.95 0 0 0 2 12a9.95 9.95 0 0 0 3.64 7.71A9.96 9.96 0 0 0 12 22c2.42 0 4.63-.86 6.36-2.29a10 10 0 0 0 1.93-2.12 9.95 9.95 0 0 0 1.71-5.6 9.96 9.96 0 0 0-2.28-6.35'/%3E%3C/svg%3E" alt="image2" />
+                    <input type="text" placeholder="Saerch products, brands and categories "/>
+                    
+                </nav>
+            </header>
+
+        </main>
+
+  )
+}
+
+export default Header
